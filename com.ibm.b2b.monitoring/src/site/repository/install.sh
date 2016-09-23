8fca99d29bbee43c32912772bda818c9a565e440
dd00f <mcduffs@ca.ibm.com>
2016-09-23 13:53:33 -0400

    Close idle performance metric files after 5 minutes.

--- com.ibm.b2b.monitoring/src/site/repository/install.sh
+++ com.ibm.b2b.monitoring/src/site/repository/install.sh
@@ -1,4 +1,4 @@
-if [ -z ${1+x} ]; then logstashserver=gdhape01.canlab.ibm.com; else logstashserver=$1; fi
+if [ -z ${1+x} ]; then logstashserver=DISABLED; else logstashserver=$1; fi
 if [ -z ${2+x} ]; then logstashserverport=2003; else logstashserverport=$2; fi
 if [ -z ${3+x} ]; then { echo "No repository URL specified, installation failed."; exit 1; }; else repositoryurl=$3; fi
 if [ -z ${4+x} ]; then installtopbeat=true else installtopbeat=$4; fi
